f2514dbc96d17cd7aba8fc135dd7a8c10d801e86
Mufaddal09 <80957173+Mufaddal09@users.noreply.github.com>
2022-12-25 12:23:39 +0530

    Add files via upload

---
+++ Source/VehicleGame/Private/Pawns/VehicleMovementComponentBoosted4w.cpp
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VehicleGame.h"
+#include "Pawns/VehicleMovementComponentBoosted4w.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+UVehicleMovementComponentBoosted4w::UVehicleMovementComponentBoosted4w(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/VehicleGame/Public/Pawns/VehicleMovementComponentBoosted4w.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "WheeledVehicleMovementComponent4W.h"
+#include "VehicleMovementComponentBoosted4w.generated.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+UCLASS()
+class UVehicleMovementComponentBoosted4w : public UWheeledVehicleMovementComponent4W
+{
+	GENERATED_UCLASS_BODY()
+};
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
